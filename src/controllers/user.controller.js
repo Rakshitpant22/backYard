51ac322988a9eb59a0bf22097afb40b44f90dd2c
Rakshitpant22 <rakshitpant22@gmail.com>
2024-09-14 15:03:22 +0530

    all setup configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js"; 
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(300).json({
+        message: " OK "
+    });
+});
+
+export { registerUser };
+
